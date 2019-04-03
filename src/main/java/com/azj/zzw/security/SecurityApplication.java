43c6c790327040738ae8ced10944ee8565f17af9
zengzhiwei <zengzhiwei@erayt.com>
2019-04-03 20:22:30 +0800

    'init'

---
+++ src/main/java/com/azj/zzw/security/SecurityApplication.java
@@ -0,0 +1,22 @@
+package com.azj.zzw.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home(){
+		return "hello spring security";
+	}
+
+}
